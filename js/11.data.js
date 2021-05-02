878abb3a12caa39f8d30100b4f042646268ec326
toomanysulkies <toomanysulkies@gmail.com>
2021-05-03 04:55:51 +0900

    0502_locate pictures

---
+++ js/11.data.js
@@ -0,0 +1,17 @@
+/*************** 글로벌 설정 *****************/
+var prds = [
+	{ id: 1, src: '../img/cherries.jpeg', title: '맛있는 체리', desc: '체리체리' },
+	{ id: 1, src: '../img/croissant.jpeg', title: '맛있는 크로와상', desc: '크로와상' },
+	{ id: 1, src: '../img/popsicle.jpeg', title: '맛있는 사탕', desc: '달콤' },
+	{ id: 1, src: '../img/salmon.jpeg', title: '맛있는 연어', desc: '연어' },
+	{ id: 1, src: '../img/steak.jpeg', title: '맛있는 스테이크', desc: '뻑뻑' },
+]
+
+/*************** 사용자 함수 *****************/
+
+
+/*************** 이벤트 등록 *****************/
+
+
+/*************** 이벤트 콜백 *****************/
+
