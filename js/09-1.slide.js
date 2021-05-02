0a1a3f84abf235cf781e40ade960bc46b842294a
Sulki Kim <78523013+toomanysulkies@users.noreply.github.com>
2021-05-02 14:47:57 +0900

    0502_commit to paste 08.slide.css from other branch

--- js/09-1.slide.js
+++ js/09-1.slide.js
@@ -27,17 +27,29 @@ function init() {
 	slideLastIdx = slideLen - 1
 	$slideWrap.css('width', slideLen * 100 + '%')
 	$slide.css('width', 100 / slideLen + '%')
-
 }
 
+function ani() {
+	$slideWrap.stop().animate({ 'left': (-idx * 100) + '%' }, aniSpeed)
+}
 /*************** 이벤트 등록 *****************/
 $slideStage.find('.bt-prev').click(onPrevClick)
 $slideStage.find('.bt-next').click(onNextClick)
 
 /*************** 이벤트 콜백 *****************/
 function onPrevClick() {
-
+	if (idx === 0) {  //idx가 0이라면 => 처음 이미지라면
+		$slideWrap.css('left', -(slideLastIdx * 100) + '%')
+		idx = slideLastIdx - 1
+	}
+	else idx--  //아니면 순서 뒤로 하나
+	ani()
 }
 function onNextClick() {
-
+	if (idx === slideLastIdx) {//idx 순서가 슬라이드의 마지막이라면
+		$slideWrap.css('left', 0)
+		idx = 1
+	}
+	else idx++ //아니라면 순서 앞으로 하나
+	ani()
 }
